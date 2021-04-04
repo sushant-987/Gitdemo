30c0603a4de2a1430515e8bf436a1adb862c2307
Sushant Godbharle <sushant@gmail.com>
2021-04-04 16:25:04 +0530

    next commit

--- src/com/gitdemo/java/B.java
+++ src/com/gitdemo/java/B.java
@@ -1,11 +1,10 @@
 package com.gitdemo.java;
 
-public class A {
+public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("Hello GitHub");
-  System.out.println("Hii Github");
+
 	}
 
 }
