768bd367a27e8d95226b81828e29cb0ca5e9955b
Sushant <sushantgod@gmail.com>
2021-04-04 17:17:47 +0530

    new file added

--- src/com/gitdemo/java/C.java
+++ src/com/gitdemo/java/C.java
@@ -1,10 +1,10 @@
 package com.gitdemo.java;
 
-public class Java {
+public class C {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("new data");
+
 	}
 
 }
